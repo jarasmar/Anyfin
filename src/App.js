6172f4c48aef94e0195b1de9d8a6092c6ea4bbf7
Jara Santamaria <jarasmar@gmail.com>
2020-10-13 14:19:27 +0200

    API call to restcountries fetchin selected data from every country

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends Component {
+  state = {
+    error: null,
+    isLoaded: false,
+    items: []
+  };
+
+  componentDidMount() {
+    axios.get("https://restcountries.eu/rest/v2/all").then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          items: result.data
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="App">
+          <h1> Countries in the World </h1>
+          <ul>
+            {items.map((item) => (
+              <li key={item.name}>
+                <h2> {item.name} </h2>
+                <p>Capital: {item.capital}</p>
+                <p> Population: {item.population} </p>
+                <p> Currency: {item.currencies[0].name} </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
 }
+
+export default App;
