f2fcf3d5f1d6ead6228ae9f9a6a32b1f1e3ff517
jarasmar <jarasmar@gmail.com>
2020-10-15 20:15:16 +0200

    Updates

---
+++ precache-manifest.bcdf49ea53fa303facb4466c7f564056.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7dcf20e31f3f840e47d318186bbe16d",
+    "url": "/Currency_Converter/index.html"
+  },
+  {
+    "revision": "9a6c843648d0b8da7274",
+    "url": "/Currency_Converter/static/css/main.6f540deb.chunk.css"
+  },
+  {
+    "revision": "0aba368e5869424d5fcc",
+    "url": "/Currency_Converter/static/js/2.b5c52bea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currency_Converter/static/js/2.b5c52bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6c843648d0b8da7274",
+    "url": "/Currency_Converter/static/js/main.b240f23f.chunk.js"
+  },
+  {
+    "revision": "6bfe597de794d76d2295",
+    "url": "/Currency_Converter/static/js/runtime-main.9b861942.js"
+  },
+  {
+    "revision": "295e8bdf393fa65767a3f936a53a4541",
+    "url": "/Currency_Converter/static/media/gilroy-extrabold-webfont.295e8bdf.woff"
+  },
+  {
+    "revision": "633444851d59f389ee2dbb2991554fe3",
+    "url": "/Currency_Converter/static/media/gilroy-extrabold-webfont.63344485.woff2"
+  },
+  {
+    "revision": "129d18482cc52887fbcb6e1a64a8e888",
+    "url": "/Currency_Converter/static/media/gilroy-light-webfont.129d1848.woff2"
+  },
+  {
+    "revision": "d30fbcc316eafe75ae7f840001f2339a",
+    "url": "/Currency_Converter/static/media/gilroy-light-webfont.d30fbcc3.woff"
+  }
+]);
